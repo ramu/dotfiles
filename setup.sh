77f7a18b40265292c513348d24ed13c473bd9ec8
ramusara <ramusara@gmail.com>
2011-06-12 23:16:45 +0900

    Updated .gitignore/setup.sh (.emacs.d)

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-# link
+########
+# link #
+########
+# zsh
 ln -sf ~/dotfiles/.zshrc  ~/.zshrc
 ln -sf ~/dotfiles/.zshenv ~/.zshenv
 ln -sf ~/dotfiles/.zsh    ~/.zsh
+
+# emacs
+ln -sf ~/dotfiles/.emacs.d ~/.emacs.d
 
