56d61d7eeaa1234a0bb40c2b477d37d9e403ee64
ramusara <ramusara@gmail.com>
2011-06-16 01:28:21 +0900

    added mercurial files.

--- setup.sh
+++ setup.sh
@@ -7,6 +7,9 @@ ln -sf ~/dotfiles/.zshrc  ~/.zshrc
 ln -sf ~/dotfiles/.zshenv ~/.zshenv
 ln -sf ~/dotfiles/.zsh    ~/.zsh
 
+# mercurial
+ln -sf ~/dotfiles/.hgrc ~/.hgrc
+
 # emacs
 ln -sf ~/dotfiles/.emacs.d ~/.emacs.d
 
