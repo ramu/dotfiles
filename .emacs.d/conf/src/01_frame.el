c65a86a1a02ac250860e27a3093878e110eb0b58
ramusara <ramusara@gmail.com>
2011-08-07 15:12:59 +0900

    use linum(disabled yalinum)

--- .emacs.d/conf/src/01_frame.el
+++ .emacs.d/conf/src/01_frame.el
@@ -32,37 +32,49 @@
 ;; フレームのタイトル指定
 (setq frame-title-format (concat "%b - emacs@" system-name))
 
+;;;;; 左側に行数表示
+(require 'linum)
+(setq linum-format "%5d ")
+(defvar mylinum-mode-name nil)
+(setq mylinum-mode-name '(emacs-lisp-mode-hook slime-mode-hook sh-mode-hook text-mode-hook
+                           php-mode-hook python-mode-hook ruby-mode-hook
+                           css-mode-hook yaml-mode-hook org-mode-hook
+                           howm-mode-hook js2-mode-hook javascript-mode-hook
+                           smarty-mode-hook html-helper-mode-hook))
+(mapc (lambda (hook-name) (add-hook hook-name (lambda () (linum-mode t)))) mylinum-mode-name)
+
+;;;;; 左側に行数表示(line-num.el) ---> 現在位置も分かる拡張版(yalinum.el)
+;;;;; 相性問題？不安定なので使用中止
 ;; scrollbarは不要(yalinumで対応)
 (toggle-scroll-bar nil)
-;; 左側に行数表示(line-num.el) ---> 現在位置も分かる拡張版(yalinum.el)
-(require 'yalinum)
-(setq yalinum-line-number-display-format "%5d")
-
-(dolist (hook (list
-               'c-mode-common-hook
+;(require 'yalinum)
+;(setq yalinum-line-number-display-format "%5d")
+;
+;(dolist (hook (list
+;               'c-mode-common-hook
 ; yalinum+anything+popwinの相性が悪い。開けなくなる
 ;  ---> 下記二つに関しては表示しない
 ;               'emacs-lisp-mode-hook
 ;               'lisp-interaction-mode-hook
-               'lisp-mode-hook
-               'java-mode-hook
-               'haskell-mode-hook
-               'sh-mode-hook
-               'cperl-mode-hook
-               'python-mode-hook
-               'ess-mode-hook
-               'inferior-ess-mode-hook
-               'lua-mode-hook
-               'html-mode-hook
-               'scala-mode-hook))
-  (add-hook hook '(lambda ()
-                   (yalinum-mode 1))))
+;               'lisp-mode-hook
+;               'java-mode-hook
+;               'haskell-mode-hook
+;               'sh-mode-hook
+;               'cperl-mode-hook
+;               'python-mode-hook
+;               'ess-mode-hook
+;               'inferior-ess-mode-hook
+;               'lua-mode-hook
+;               'html-mode-hook
+;               'scala-mode-hook))
+;  (add-hook hook '(lambda ()
+;                   (yalinum-mode 1))))
 ;(global-yalinum-mode t)
 ;;yalinum色設定
-(set-face-background 'yalinum-face "#000000")
-(set-face-foreground 'yalinum-face "#009900")
-(set-face-background 'yalinum-bar-face "#003300")
-(set-face-foreground 'yalinum-bar-face "#00FF00")
+;(set-face-background 'yalinum-face "#000000")
+;(set-face-foreground 'yalinum-face "#009900")
+;(set-face-background 'yalinum-bar-face "#003300")
+;(set-face-foreground 'yalinum-bar-face "#00FF00")
 
 ;; モードライン
 (setq-default mode-line-format
