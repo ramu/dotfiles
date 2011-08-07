2f96e33a5196bd44f4c2f490c9169fab3953df15
ramusara <ramusara@gmail.com>
2011-08-07 16:00:29 +0900

    added JUNIT path

--- .zsh/001_export.zsh
+++ .zsh/001_export.zsh
@@ -5,4 +5,6 @@ export EDITOR=/Application/MacVim.app/Contents/MacOS/Vim
 export JAVA_OPTS="-XstartOnFirstThread -d32 -Dfile.encoding=UTF-8"
 export LANG=ja_JP.UTF-8
 export TZ='JST-9'
+export JUNIT_HOME='/usr/local/java/junit/'
+export CLASSPATH="${CLASSPATH}:$JUNIT_HOME/junit.jar"
 
