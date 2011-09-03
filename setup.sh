c58b261d877e8ee5d1b2f8da2dd7687488a0247d
ramusara <ramusara@gmail.com>
2011-09-03 18:03:51 +0900

    perlbrew

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+cd
+
 ########
 # link #
 ########
@@ -20,3 +22,14 @@ ln -sf ~/dotfiles/.emacs.d ~/.emacs.d
 
 # screen
 ln -sf ~/dotfiles/.screenrc ~/.screenrc
+
+############
+# perlbrew #
+############
+curl -L http://xrl.us/perlbrewinstall | bash
+~/perl5/perlbrew/bin/perlbrew init
+source ~/perl5/perlbrew/etc/bashrc
+perlbrew install -j 5 perl-5.14.1
+perlbrew switch perl-5.14.1
+perlbrew install-cpanm
+
