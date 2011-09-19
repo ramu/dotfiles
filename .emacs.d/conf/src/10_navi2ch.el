244073b35aef4b2e3ea0c4b45093c7bb37149014
ramusara <ramusara@gmail.com>
2011-09-19 13:27:00 +0900

    added navi2ch

---
+++ .emacs.d/conf/src/10_navi2ch.el
@@ -0,0 +1,6 @@
+;;; 10_navi2ch.el ---
+(autoload 'navi2ch "navi2ch" "Navigator for 2ch for Emacs" t)
+
+(setq navi2ch-mona-enable t)
+(setq navi2ch-mona-face-variable 'navi2ch-mona16-face)
+(setq navi2ch-article-auto-range nil)
