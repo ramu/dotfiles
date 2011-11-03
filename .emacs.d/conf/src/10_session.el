ab6b4638c813e3e8493867d06aad5be360bbcf51
ramusara <ramusara@gmail.com>
2011-11-03 21:47:59 +0900

    modified session-save-file

--- .emacs.d/conf/src/10_session.el
+++ .emacs.d/conf/src/10_session.el
@@ -1,5 +1,6 @@
-;;; 10_session.el --- 
-(when (require 'session nil t)
+;;; 10_session.el ---
+(require 'session)
+
 (setq session-initialize '(de-saveplace session keys menus places)
       session-globals-include '((kill-ring 50)
                                 (session-file-alist 500 t)
@@ -7,5 +8,7 @@
 
 (setq session-globals-max-string 100000000)
 (setq history-length t)
-(add-hook 'after-init-hook 'session-initialize))
+(setq session-save-file (expand-file-name "./.emacs.d/var/.session"))
+
+(add-hook 'after-init-hook 'session-initialize)
 
