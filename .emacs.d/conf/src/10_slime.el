c10a1bfc73960c3b0b21d52aef8e2be17ae68beb
ramusara <ramusara@gmail.com>
2011-11-17 12:48:34 +0900

    update(del comment/add hyperspec)

--- .emacs.d/conf/src/10_slime.el
+++ .emacs.d/conf/src/10_slime.el
@@ -7,9 +7,6 @@
 (add-hook 'lisp-mode-hook (lambda ()
                             (slime-mode t)
                             ))
-;(add-hook 'repl-mode-hook (lambda ()
-;                            (setq show-trailing-whitespace nil)
-;                            ))
 
 ;; auto-complete
 (require 'ac-slime)
@@ -22,7 +19,20 @@
 (require 'hyperspec)
 (setq common-lisp-hyperspec-root         (concat "file://" (expand-file-name "~/.emacs.d/share/slime/HyperSpec/"))
       common-lisp-hyperspec-symbol-table (expand-file-name "~/.emacs.d/share/slime/HyperSpec/Data/Map_Sym.txt"))
-
+(defadvice common-lisp-hyperspec
+  (around hyperspec-lookup-w3m () activate)
+  (let* ((window-configuration (current-window-configuration))
+         (browse-url-browser-function
+          `(lambda (url new-window)
+             (w3m-browse-url url nil)
+             (let ((hs-map (copy-keymap w3m-mode-map)))
+               (define-key hs-map (kbd "q")
+                 (lambda ()
+                   (interactive)
+                   (kill-buffer nil)
+                   (set-window-configuration ,window-configuration)))
+               (use-local-map hs-map)))))
+    ad-do-it))
 
 
 (slime-setup '(slime-repl slime-fancy slime-banner))
