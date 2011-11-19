7b688e088ecba45e0dbd934372885f9932c17bec
ramusara <ramusara@gmail.com>
2011-11-19 23:42:46 +0900

    mkdir .emacs.d/private

--- .emacs.d/conf/src/10_bm.el
+++ .emacs.d/conf/src/10_bm.el
@@ -2,11 +2,19 @@
 (setq-default bm-buffer-persistence nil)
 (setq bm-restore-repository-on-load t)
 (require 'bm)
+
+(add-hook 'after-init-hook 'bm-repository-load)
 (add-hook 'find-file-hooks 'bm-buffer-restore)
 (add-hook 'kill-buffer-hook 'bm-buffer-save)
 (add-hook 'after-save-hook 'bm-buffer-save)
 (add-hook 'after-revert-hook 'bm-buffer-restore)
+(add-hook 'kill-emacs-hook '(lambda nil
+                              (bm-buffer-save-all)
+                              (bm-repository-save)))
 (add-hook 'vc-before-checkin-hook 'bm-buffer-restore)
+
+(setq bookmark-default-file "~/.emacs.d/private/.emacs.bmk")
+
 (global-set-key (kbd "M-@") 'bm-toggle)
 (global-set-key (kbd "M-[") 'bm-previous)
 (global-set-key (kbd "M-]") 'bm-next)
