a497831b822af4504e81e07c9bc7bcd1f2037a71
ramusara <ramusara@gmail.com>
2012-01-08 23:35:53 +0900

    disable ddskk

--- .emacs.d/conf/src/90_input.el
+++ .emacs.d/conf/src/90_input.el
@@ -76,17 +76,17 @@
 ;;; ddskk.el ---
 ;; Daredevil SKK (DDSKK) は Emacs Lisp 版の SKK です。
 ;; http://openlab.ring.gr.jp/skk/ddskk-ja.html
-(require 'skk-autoloads)
-(global-set-key "\C-x\C-j" 'skk-mode)
-(global-set-key "\C-xj" 'skk-auto-fill-mode)
-(global-set-key "\C-xt" 'skk-tutorial)
+;(require 'skk-autoloads)
+;(global-set-key "\C-x\C-j" 'skk-mode)
+;(global-set-key "\C-xj" 'skk-auto-fill-mode)
+;(global-set-key "\C-xt" 'skk-tutorial)
 
-(setq skk-sticky-key ";")
-(setq skk-show-inline 'vertical)
-(setq skk-dcomp-multiple-activate t)
+;(setq skk-sticky-key ";")
+;(setq skk-show-inline 'vertical)
+;(setq skk-dcomp-multiple-activate t)
 
-(setq skk-large-jisyo "/Applications/Emacs.app/Contents/Resources/etc/skk/SKK-JISYO.L")
-(skk-mode)
+;(setq skk-large-jisyo "/Applications/Emacs.app/Contents/Resources/etc/skk/SKK-JISYO.L")
+;(skk-mode)
 
 
 ;;; sekka.el ---
