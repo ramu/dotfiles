945b138e14e3822d97b57e2a04b17e4074c36b57
ramusara <ramusara@gmail.com>
2012-02-26 22:57:03 +0900

    private

--- .emacs.d/conf/src/08_shell.el
+++ .emacs.d/conf/src/08_shell.el
@@ -1,4 +1,5 @@
 ;;;; 08_shell.el
+; ほとんどprivate側
 
 ;; shell
 (defun skt:shell ()
@@ -9,22 +10,4 @@
 (setq shell-file-name (skt:shell))
 (setenv "SHELL" shell-file-name)
 (setq explicit-shell-file-name shell-file-name)
-
-;;; shell-pop.el
-(require 'shell-pop)
-(shell-pop-set-internal-mode "ansi-term")
-(shell-pop-set-internal-mode-shell "/bin/zsh")
-(shell-pop-set-window-height 60) ;the number for the percentage of the selected window. if 100, shell-pop use the whole of selected window, not spliting.
-(shell-pop-set-window-position "bottom") ;shell-pop-up position. You can choose "top" or "bottom".
-(global-set-key (kbd "C-c t") 'shell-pop)
-
-;;; shell-toggle ---
-(require 'shell-toggle)
-(autoload 'shell-toggle "shell-toggle" "Toggles between the *shell* buffer and whatever buffer you are editing." t)
-(autoload 'shell-toggle-cd "shell-toggle" "Pops up a shell-buffer and insert a \"cd <file-dir>\" command." t)
-(global-set-key "\C-ct" 'shell-toggle)
-(global-set-key "\C-cd" 'shell-toggle-cd)
-(add-hook 'term-mode-hook '(lambda ()
-                               (define-key term-raw-map "\C-z"
-                               (lookup-key (current-global-map) "\C-z"))))
 
