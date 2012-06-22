1801ad3103949f2834708c3dbd70ac9ab0244938
ramusara <ramusara@gmail.com>
2012-06-23 01:02:29 +0900

    add kill-current-buffer

--- .emacs.d/conf/src/98_keybindings.el
+++ .emacs.d/conf/src/98_keybindings.el
@@ -24,6 +24,7 @@
 (global-set-key (kbd "C-c t") 'multi-term)             ; Terminal
 ;(global-set-key (kbd "C-t") 'other-window-or-split)   ; 01_frame.el
 (global-set-key (kbd "C-c l") 'se/make-summary-buffer) ; summarye
+(global-set-key (kbd "C-c k") '(lambda () (interactive) (kill-buffer (current-buffer))))
 (global-set-key (kbd "C-c C-k") '(lambda () (interactive) (kill-line 0)))
 (global-set-key (kbd "C-M-i") 'indent-region)
 (global-set-key (kbd "C-M-g") 'anything-grep)
