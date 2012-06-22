635fdcfb3f315ec02a0de3a6e04bacc8720897a2
ramusara <ramusara@gmail.com>
2012-06-23 00:35:44 +0900

    change color

--- .emacs.d/conf/src/90_highlight.el
+++ .emacs.d/conf/src/90_highlight.el
@@ -87,17 +87,17 @@
 (add-hook 'shell-mode-hook 'rainbow-delimiters-mode)
 (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
 ;; colors
-(set-face-foreground 'rainbow-delimiters-depth-1-face  "#EFEFEF")  ; Grey
-(set-face-foreground 'rainbow-delimiters-depth-2-face  "#55FF55")  ; Green
-(set-face-foreground 'rainbow-delimiters-depth-3-face  "#5555FF")  ; Blue
-(set-face-foreground 'rainbow-delimiters-depth-4-face  "#FF5555")  ; Red
-(set-face-foreground 'rainbow-delimiters-depth-5-face  "#FFFF55")  ; Yellow
-(set-face-foreground 'rainbow-delimiters-depth-6-face  "#FF55FF")  ; Purple
-(set-face-foreground 'rainbow-delimiters-depth-7-face  "#55FFFF")  ; light-blue
-(set-face-foreground 'rainbow-delimiters-depth-8-face  "#BBBBBB")  ; Grey...
-(set-face-foreground 'rainbow-delimiters-depth-9-face  "#999999")  ; Grey....
-(set-face-foreground 'rainbow-delimiters-depth-10-face "#777777")  ; Grey...
-(set-face-foreground 'rainbow-delimiters-depth-11-face "#555555")  ; Grey...
+(set-face-foreground 'rainbow-delimiters-depth-1-face  "#4455AA")  ; Grey
+(set-face-foreground 'rainbow-delimiters-depth-2-face  "#33AA33")  ; Green
+(set-face-foreground 'rainbow-delimiters-depth-3-face  "#3333AA")  ; Blue
+(set-face-foreground 'rainbow-delimiters-depth-4-face  "#AA3333")  ; Red
+(set-face-foreground 'rainbow-delimiters-depth-5-face  "#AAAA33")  ; Yellow
+(set-face-foreground 'rainbow-delimiters-depth-6-face  "#AA33AA")  ; Purple
+(set-face-foreground 'rainbow-delimiters-depth-7-face  "#33AAAA")  ; light-blue
+(set-face-foreground 'rainbow-delimiters-depth-8-face  "#777777")  ; Grey...
+(set-face-foreground 'rainbow-delimiters-depth-9-face  "#666666")  ; Grey....
+(set-face-foreground 'rainbow-delimiters-depth-10-face "#555555")  ; Grey...
+(set-face-foreground 'rainbow-delimiters-depth-11-face "#444444")  ; Grey...
 (set-face-foreground 'rainbow-delimiters-depth-12-face "#333333")  ; Grey...
-;; test -> (((((((((((())))))))))))
+; test -> (((((((((((())))))))))))
 
