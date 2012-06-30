e2c484a4756ff8347c6cb8f811d11b4941329e65
ramusara <ramusara@gmail.com>
2012-07-01 00:02:03 +0900

    grails & griffon

--- .zsh/000_path.zsh
+++ .zsh/000_path.zsh
@@ -2,14 +2,17 @@
 # path
 PATH=/Users/ramusara/bin
 PATH=$PATH:/usr/local/bin
+PATH=$PATH:/usr/local/sbin
 PATH=$PATH:/opt/local/bin
 PATH=$PATH:/usr/bin
+PATH=$PATH:/usr/sbin
 PATH=$PATH:/bin
 PATH=$PATH:/sbin
-PATH=$PATH:/usr/sbin
 PATH=$PATH:/Users/ramusara/scala/bin
 PATH=$PATH:/Users/ramusara/Android/android-ndk-r5b
 PATH=$PATH:/Library/Java/ant/bin
+PATH=$PATH:/usr/share/grails/bin
+PATH=$PATH:/usr/share/griffon/bin
 PATH=/Users/ramusara/.rbenv/bin:$PATH
 
 eval "$(rbenv init -)"
--- .zsh/900_java.zsh
+++ .zsh/900_java.zsh
@@ -3,4 +3,6 @@
 JAVA_HOME=/Library/Java/Home
 ANT_HOME=/Library/Java/ant
 
+GRAILS_HOME=/usrshare/grails; export GRAILS_HOME
+GRIFFON_HOME=/usr/share/griffon; export GRIFFON_HOME
 
