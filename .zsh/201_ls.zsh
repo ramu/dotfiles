374118af6a42658213de033a43c0bac2b141bacd
ramusara <ramusara@gmail.com>
2012-11-15 00:02:45 +0900

    set LISTMAX

--- .zsh/201_ls.zsh
+++ .zsh/201_ls.zsh
@@ -3,6 +3,9 @@
 export LSCOLORS=exfxcxdxbxegedabagacad
 zstyle ':completion:*' list-colors ''
 
+# 補完候補が多い時に下記値より多くない場合は確認メッセージ出さない
+LISTMAX=10000
+
 # ls /usr/local/etcなどを以下のようにC-wで単語毎に削除
 #   default : ls /usr/local ---> ls /usr/ ---> ls /usr ---> / 
 #   この設定: ls /usr/local ---> ls /usr/ ---> ls 
