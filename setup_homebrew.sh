76d14eb58aa9bdb060c385802288372fb77b1065
ramu <ramusara@gmail.com>
2014-04-29 08:01:53 +0900

    homebrew用初期設定ファイル追加(コマンド叩きたくなるかもしれないのでbrewfileではなくshell)

---
+++ setup_homebrew.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+brew update
+
+brew upgrade
+
+brew tap phinze/homebrew-cask
+
+brew install brew-cask
+brew install eck
+brew install autoconf
+brew install bazaar
+brew install bdw-gc
+brew install cmigemo
+brew install curl
+brew install cvs
+brew install fontconfig
+brew install fontforge
+brew install freetype
+brew install gdbm
+brew install gettext
+brew install git
+brew install git-flow
+brew install hub
+brew install jpeg
+brew install jq
+brew install libevent
+brew install libpng
+brew install libtiff
+brew install markdown
+brew install maven
+brew install mercurial
+brew install nkf
+brew install nmap
+brew install node
+brew install openssl
+brew install pcre
+brew install pkg-config
+brew install putty
+brew install rbenv
+brew install ricty
+brew install ruby-build
+brew install tig
+brew install tmux
+brew install tree
+brew install vim
+brew install w3m
+brew install wget
+brew install z
+brew install zsh
+
+brew cask install adobe-reader
+brew cask install alfred
+brew cask install dropbox
+brew cask install github
+brew cask install google-chrome
+brew cask install google-drive
+brew cask install google-hangouts
+brew cask install iterm2
+brew cask install skype
+brew cask install sourcetree
+brew cask install vagrant
+brew cask install virtualbox
