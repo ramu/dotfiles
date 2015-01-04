d1920c98b8514e776e04f4218027a457b910c8df
ramu <ramusara@gmail.com>
2015-01-04 17:02:16 +0900

    helm-c-yasnippet キー変更、およびキー表示

--- .emacs.d/conf/src/10_helm.el
+++ .emacs.d/conf/src/10_helm.el
@@ -65,7 +65,8 @@
 
   (my-require-and-when 'helm-c-yasnippet
     (setq helm-yas-space-match-any-greedy t)
-    (global-set-key (kbd "C-c C-y") 'helm-yas-complete)
+    (setq helm-yas-display-key-on-candidate t)
+    (global-set-key (kbd "C-c y") 'helm-yas-complete)
     (yas-global-mode 1)
     (yas-load-directory "/Users/ramusara/.emacs.d/plugins/yasnippet/"))
 
