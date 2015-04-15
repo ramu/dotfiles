1e33af128af3a60167cb8e1b21f939ac3b9eee4c
ramu <ramusara@gmail.com>
2015-04-15 12:52:30 +0900

    emacswiki繋がらない時にフリーズしたままとなるので、(auto-install-update-emacswiki-package-name t) 削除(タイムアウトも検討したが、普段updateする必要性無いので削除)

--- .emacs.d/conf/src/04_package.el
+++ .emacs.d/conf/src/04_package.el
@@ -7,7 +7,6 @@
 (my-require-and-when 'auto-install
   (add-to-list 'load-path auto-install-directory)
   (setq auto-install-directory "~/.emacs.d/elisp/src/")
-  (auto-install-update-emacswiki-package-name t)
   (auto-install-compatibility-setup)
   (setq ediff-window-setup-function 'ediff-setup-windows-plain))
 
