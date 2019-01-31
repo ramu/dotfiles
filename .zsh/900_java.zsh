eb5b8ce7ebee78dcba22a17878d3c14a58e2a392
ramu <ramusara@gmail.com>
2019-01-31 17:39:22 +0900

    fix paths

--- .zsh/900_java.zsh
+++ .zsh/900_java.zsh
@@ -3,6 +3,6 @@
 JAVA_HOME=/Library/Java/Home
 ANT_HOME=/Library/Java/ant
 
-GRAILS_HOME=/usrshare/grails; export GRAILS_HOME
+GRAILS_HOME=/usr/share/grails; export GRAILS_HOME
 GRIFFON_HOME=/usr/share/griffon; export GRIFFON_HOME
 
