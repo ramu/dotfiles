d5ad8e50db67b307855b5104f3bcfcdd69eca3f7
ramu <ramusara@gmail.com>
2022-08-26 17:43:29 +0900

    add kube-ps1

--- .zsh/002_source.zsh
+++ .zsh/002_source.zsh
@@ -3,6 +3,8 @@
 source ~/perl5/perlbrew/etc/bashrc
 [[ -s ~/.pythonbrew/etc/bashrc ]] && source ~/.pythonbrew/etc/bashrc
 source ~/.gvm/scripts/gvm
+[[ -s ~/.zsh/modules/kube-ps1/kube-ps1.sh ]] && source ~/.zsh/modules/kube-ps1/kube-ps1.sh
+
 
 [[ -s ~/.cargo/env ]] && source ~/.cargo/env
 
