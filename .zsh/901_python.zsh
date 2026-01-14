d113137eb6f1a9d16a537470295abcd790268f6f
ramu <ramusara@gmail.com>
2026-01-14 10:19:13 +0900

    古い言語環境設定の削除

--- .zsh/901_python.zsh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-if [ -f /usr/local/bin/virtualenvwrapper.sh ]; then
-   export WORKON_HOME=$HOME/.virtualenvs
-   export VIRTUALENVWRAPPER_PYTHON=$HOME_DIR/.pythonbrew/pythons/Python-3.2/bin/python
-   source /usr/local/bin/virtualenvwrapper.sh
-fi
